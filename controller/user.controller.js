6369ad54b451ac2fd3a938de080d8202e2c2d9e4
Intuneteq <tobiolanitori@gmail.com>
2023-12-09 05:56:42 +0000

    feat: user googlespreadsheet

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -3,6 +3,9 @@ const Agency = require("../models/agency.model");
 
 const Joi = require("joi");
 const mongoose = require("mongoose");
+const { GoogleSpreadsheet } = require("google-spreadsheet");
+
+const creds = require("../client_seceret.json");
 
 const createUser = async (req, res) => {
   const validatePayload = (data) => {
@@ -29,14 +32,41 @@ const createUser = async (req, res) => {
   const { error, value } = validatePayload(req.body);
   if (error) return res.status(400).json({ message: error.message });
 
+  let agency;
   if (value.agencyId) {
     if (!mongoose.Types.ObjectId.isValid(value.agencyId))
       return res.status(400).json({ message: "Invalid Agency Id" });
 
-    const isAgencyExists = await Agency.exists({ _id: value.agencyId });
+    agency = await Agency.findById(value.agencyId);
 
-    if (!isAgencyExists)
-      return res.status(404).json({ message: "Agency not found" });
+    if (!agency) return res.status(404).json({ message: "Agency not found" });
+  }
+
+  const spreadSheetId = process.env.SPREADSHEET_ID;
+  const doc = new GoogleSpreadsheet(spreadSheetId);
+
+  try {
+    await doc.useServiceAccountAuth(creds);
+    await doc.getInfo();
+    const sheet = doc.sheetsByTitle["users"];
+    await sheet.addRow({
+      firstName: value.firstName,
+      lastName: value.lastName,
+      contactNumber: value.contactNumber,
+      address: value.address,
+      postcode: value.postcode,
+      benefit: value.benefit ? "YES" : "NO",
+      available: value.available ? "YES" : "NO",
+      consent: !value.agencyId ? " " : value.benefit ? "YES" : "NO",
+      benefits: value.benefits.join(","),
+      houseDemography: value.houseDemography,
+      ethnicity: value.ethnicity,
+      age: value.age,
+      agency: agency.name,
+      timeOfCollection: value.timeOfCollection,
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
   }
 
   const user = new User({ ...value, agency: value.agencyId });
